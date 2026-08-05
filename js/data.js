75d606fb58b1c776d9057ca78ee11c0058860daf
syrsln <syrsln@gmail.com>
2026-08-05 15:30:02 +0300

    Replace 🩷 (Unicode 13+) with 💖 (widely supported) for Pink color

--- js/data.js
+++ js/data.js
@@ -55,7 +55,7 @@ const Data = {
     { id: 'yellow', word: 'Yellow', emoji: '🟡', fa: 'زرد',      color: '#ffeb3b' },
     { id: 'orange', word: 'Orange', emoji: '🟠', fa: 'نارنجی',   color: '#ff9800' },
     { id: 'purple', word: 'Purple', emoji: '🟣', fa: 'بنفش',     color: '#9c27b0' },
-    { id: 'pink',   word: 'Pink',   emoji: '🩷', fa: 'صورتی',    color: '#ec407a' },
+    { id: 'pink',   word: 'Pink',   emoji: '💖', fa: 'صورتی',    color: '#ec407a' },
     { id: 'brown',  word: 'Brown',  emoji: '🟤', fa: 'قهوه‌ای', color: '#795548' },
     { id: 'black',  word: 'Black',  emoji: '⚫', fa: 'سیاه',     color: '#212121' },
     { id: 'white',  word: 'White',  emoji: '⚪', fa: 'سفید',     color: '#9e9e9e' }
